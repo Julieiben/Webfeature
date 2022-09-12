d92ad7345093f9f7b78fae20e144c9fbcb039c60
Ida Viborg <idax531r@stud.kea.dk>
2022-09-12 19:56:21 +0200

    Indhold i sektioner

--- src/main.js
+++ src/main.js
@@ -1,3 +1,23 @@
 import "./style.css";
 /* Import af Motion One bibliotek */
 import { animate, stagger, inView, scroll, timeline } from "motion";
+
+/*HORIZONTAL LISTER*/
+const items = document.querySelectorAll("#horisontalliste li");
+scroll(
+  animate("#horisontalliste", {
+    transform: ["none", `translateX(-${items.length - 1}00vw)`],
+  }),
+  { target: document.querySelector("#horisontalscrollsection") }
+);
+
+const pics = document.querySelectorAll("#horisontalliste_2 li");
+scroll(
+  animate("#horisontalliste_2", {
+    transform: ["none", `translateX(-${pics.length - 1}00vw)`],
+  }),
+  { target: document.querySelector("#horisontalscrollsection_2") }
+);
+
+/*SCROLL BAR*/
+scroll(animate(".progress-bar", { scaleX: [0, 1] }));
