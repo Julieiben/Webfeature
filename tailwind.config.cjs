8aa62628933ff8ee6e51b5f8c200bba2007730b0
Ida Viborg <idax531r@stud.kea.dk>
2022-09-13 10:06:54 +0200

    styling

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,vue,js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      marine: "#2447FF",
+      neonpink: "#DB00FF",
+      black: "#000000",
+      white: "#ffffff",
+    },
     extend: {
       fontFamily: {
         rexlia: "'relaxia', sans-serif",
